7745f116199facb54162b5339d10df076e57594a
Geethika Tiramdas <geethika.tiramdas@tuhh.de>
2023-05-24 19:22:01 +0200

    Setup campaign page modified

--- ui/src/app/components/admin-set-campaign/admin-set-campaign.component.ts
+++ ui/src/app/components/admin-set-campaign/admin-set-campaign.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-admin-set-campaign',
@@ -10,13 +11,32 @@ import { Router } from '@angular/router';
 export class AdminSetCampaignComponent {
   setup = true;
   setupform: FormGroup;
-  candidates: string[] = ['ABC', 'XYZ', 'ASDF'];
+  candidates: { [key: string]: any }[] = [];
+  @ViewChild(MatTable) table!: MatTable<any>;
 
   constructor(private router: Router, private fb: FormBuilder) {
     this.setupform = this.fb.group({
-      campName: ['', Validators.required, Validators.pattern('^[0-9]*$')],
-      candidates: ['', Validators.required],
+      campName: ['', Validators.required, Validators.pattern('^[a-zA-Z,]*$')],
+      candidateName: ['', Validators.required, Validators.pattern('^[a-zA-Z,]*$')],
+      candidateParty: ['', Validators.required, Validators.pattern('^[a-zA-Z,]*$')],
+      candidateText: ['', Validators.required, Validators.pattern('^[a-zA-Z,]*$')],
     });
+  }
+
+  dummy: { [key: string]: any } = {};
+  showtable: boolean = false;
+  displayedColumns: string[] = ['candidateName', 'candidateParty', 'candidateText'];
+  addCandidates(){
+    this.dummy = { 'candidateName': this.setupform.get('candidateName')?.value, 
+    'candidateParty': this.setupform.get('candidateParty')?.value,
+    'candidateText': this.setupform.get('candidateText')?.value };
+    this.candidates.push(this.dummy);
+    console.log(this.candidates);
+    this.setupform.get('candidateName')?.reset();
+    this.setupform.get('candidateParty')?.reset();
+    this.setupform.get('candidateText')?.reset();
+    this.showtable = true;
+    this.table.renderRows();
   }
 
   setupCampaign() {
