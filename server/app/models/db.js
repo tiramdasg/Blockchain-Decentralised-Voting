90b86923263f2d29ea491f34b0f3a468c9a624d4
sujay <sujay.chavan@tuhh.de>
2023-06-05 01:30:13 +0200

    create table automatically (for docker)

--- server/app/models/db.js
+++ server/app/models/db.js
@@ -10,9 +10,15 @@ const connection = mysql.createConnection({
 });
 
 // open the MySQL connection
+try {
 connection.connect(error => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
+  connection.query("CREATE TABLE IF NOT EXISTS voters (`VoterID` int NOT NULL, `VoterName` varchar(45) NOT NULL, `Email` varchar(45) NOT NULL, `Password` varchar(100) DEFAULT NULL, `isAdmin` tinyint(1) DEFAULT NULL, `public_key` varchar(50) NOT NULL, `isAprroved` tinyint(1) DEFAULT NULL, PRIMARY KEY (`VoterID`))")
 });
+}
+catch(err) {
+  console.log("ERRoR is " +err)
+}
 
 module.exports = connection;
